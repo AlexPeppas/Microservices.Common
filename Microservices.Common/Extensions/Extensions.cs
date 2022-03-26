9b5b2fc7b60e680fadeaee22ba313325bc91f487
AlexPeppas <apeppas@deloitte.gr>
2022-03-26 18:25:47 +0200

    Enhanced custom httpClient with AddTransientHttpErrorPolicy for wait and retry strategy and .AddPolicyHandler to throw timeoutRejectedException after predefined timeout property

--- Microservices.Common/Extensions/Extensions.cs
+++ Microservices.Common/Extensions/Extensions.cs
@@ -3,13 +3,17 @@ using Microservices.Common.Client;
 using Microservices.Common.Interfaces;
 using Microservices.Common.Repository;
 using Microservices.Common.Settings;
+using Microservices.Common.Types;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 using Polly;
+using Polly.Timeout;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 
@@ -17,19 +21,45 @@ namespace Microservices.Common.Extensions
 {
     public static class Extensions
     {
-        public static IServiceCollection AddCustomHttpClient(this IServiceCollection services,string baseUrl,IDictionary<string,string> headers=null)
+        public static IServiceCollection AddCustomHttpClient(this IServiceCollection services,HttpClientParameters clientParams)
         {
             /*services.AddSingleton<IHttpCustomClient>(httpCustomClient =>
             {
                 return  new HttpCustomClient(baseUrl, headers ?? null);   
             });*/
-            
-            //AddHttpClient binds the Interface with the Method and set the baseAddress of the client
-            //in linq. AddPolicyHandler dictates the user the seconds to timeout
+
+            //AddHttpClient extension maps the Interface with the Method and set the baseAddress of the client
+            //in linq and the custom headers.
+            Random jitterer = new Random();
             services.AddHttpClient<IHttpCustomClient, HttpCustomClient>(client =>
             {
-                client.BaseAddress = new System.Uri(baseUrl);
-            }).AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+                client.BaseAddress = new Uri(clientParams.baseUrl);
+                if (clientParams.headers != null && clientParams.headers.Count > 0)
+                {
+                    foreach (var header in clientParams.headers)
+                    {
+                        client.DefaultRequestHeaders.TryAddWithoutValidation(header.Key, header.Value);
+                    }
+                }
+            })
+            //AddTransientHttpErrorPolicy receives a builder which uses Or<> to handle timeout exception and wait and retry on the following logic
+            //first input is the retries count
+            //second is the retryAttempt which dictates the expo backoff time to wait between each retry.
+            //third is an Action delegate onRetry which gets an instance of buildServiceProvider so we can leverage getService from provider and get ILogger interface
+            //Logs in console the warning message (DO NOT USE IN PRODUCTION, IT'S SIMULATION LOGGING)
+            .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+                5, //retries count
+                retryAttempt => TimeSpan.FromSeconds(Math.Pow(2,retryAttempt)) //2^1 next retry 2^2 , 2^3 and so on ...
+                                +TimeSpan.FromMilliseconds(jitterer.Next(0,1000)),//(plus some randomness not to fire Catalog from multiple clients using Inventory service on exact same time)
+                onRetry: (outcome, timeSpan, retryAttempt) => 
+                {
+                    var serviceProvider = services.BuildServiceProvider();
+                    serviceProvider.GetService<ILogger<HttpCustomClient>>()
+                    ?.LogWarning($"Delaying for {timeSpan.TotalSeconds} seconds, then making retry {retryAttempt}");
+                }
+            ))
+            //AddPolicyHandler dictates the client to throw TimeoutRejectedException on specified input timeoutPolicy param.
+            .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(clientParams.timeoutPolicy));
 
             return services;
         }
---
+++ Microservices.Common/Types/HttpClientParameters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservices.Common.Types
+{
+    public class HttpClientParameters
+    {
+        public HttpClientParameters()
+        {
+            headers = new Dictionary<string, string>(); //initialize dictionary
+            timeoutPolicy = 3; //default time
+        }
+
+        public string baseUrl { get; set; }
+
+        public Dictionary<string, string> headers { get; set; }
+
+        /// <summary>
+        /// client's timeout policy in seconds
+        /// </summary>
+        public int timeoutPolicy { get; set; }
+    }
+}
